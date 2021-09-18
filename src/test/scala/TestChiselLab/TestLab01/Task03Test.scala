e382fd5102dd8400f010c78c591c0a34adb09874
SaadAliHafiz <59177839+SaadAliHafiz@users.noreply.github.com>
2021-09-18 17:24:09 +0500

    Add files via upload

---
+++ src/test/scala/TestChiselLab/TestLab01/Task03Test.scala
@@ -0,0 +1,19 @@
+package example
+
+import org.scalatest._
+import chiseltest._
+import chisel3._
+
+import chiseltest.experimental.TestOptionBuilder._
+import chiseltest.internal.VerilatorBackendAnnotation
+
+class Task03Test extends FreeSpec with ChiselScalatestTester {
+"Task 03 Test" in {
+    
+    test(new Task03(8, 255)).withAnnotations(Seq(VerilatorBackendAnnotation)) { c =>
+
+    c.clock.step(20)
+
+    }
+}
+}
