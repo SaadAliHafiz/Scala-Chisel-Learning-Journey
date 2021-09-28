4f959a49442aafb13ec06eeb83ebb5ea88137595
SaadAliHafiz <59177839+SaadAliHafiz@users.noreply.github.com>
2021-09-28 15:46:47 +0500

    Add files via upload

---
+++ src/main/scala/ChiselLabs/Lab07/Ex1.scala
@@ -0,0 +1,15 @@
+package example
+import chisel3._
+import chisel3.util._
+
+class Ex1 extends Module{
+    val io = IO(new Bundle{
+        val in = Vec(2,Flipped(Decoupled(UInt(32.W))))
+        val out = Decoupled(UInt(32.W))})
+    val q1 = Queue(io.in(0))
+    val q2 = Queue(io.in(1))
+    val arbiter = Module(new Arbiter(UInt(),2))
+    arbiter.io.in(0) <> q1
+    arbiter.io.in(1) <> q2
+    io.out <> arbiter.io.out
+}
