ce4b3a0801888d490e57f26e91366401e175dbd5
Saad Ali <59177839+SaadAliHafiz@users.noreply.github.com>
2021-10-04 22:04:30 -0700

    Add files via upload

--- src/test/scala/TestChiselLab/TestLab07/Task02Test.scala
+++ src/test/scala/TestChiselLab/TestLab07/Task02Test.scala
@@ -10,13 +10,23 @@ import chiseltest.internal.VerilatorBackendAnnotation
 
 class Task02Test extends FreeSpec with ChiselScalatestTester {
 
-  "Lab 7 Task 2" in {
+  "Task 2" in {
   test(new Task02).withAnnotations(Seq(VerilatorBackendAnnotation)) { c =>
       c.io.start.poke(1.B)
       c.io.in.poke(1.U)
-      c.clock.step(100)
-      c.io.out.expect(0.U)
-      
+      c.clock.step(1)
+      c.io.start.poke(0.B)
+      c.io.in.poke(0.U)
+      c.clock.step(1)
+      c.io.start.poke(1.B)
+      c.io.in.poke(1.U)
+      c.clock.step(1)
+      c.io.start.poke(1.B)
+      c.io.in.poke(0.U)
+      c.clock.step(1)
+      c.io.start.poke(1.B)
+      c.io.in.poke(1.U)
+      c.clock.step(1)
     }
   }
 }
